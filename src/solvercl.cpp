247f45fdbdbc29d26efba8e40d575db991fb8c7b
Nicholas Kostelnik <nkostelnik@gmail.com>
2012-11-25 23:17:37 +0000

    ignores and working on windows

--- src/solvercl.cpp
+++ src/solvercl.cpp
@@ -8,17 +8,69 @@
 
 #define DATA_SIZE (1024)
 
-void SolverCL::init(size_t bufferSize) {
 
+static void check_error(cl_int error, char* name) {
+  if (error != CL_SUCCESS) {
+    fprintf(stderr, "Non-successful return code %d for %s.  Exiting.\n", error, name);
+    exit(1);
+  }
+}
+
+int printPlatforms()
+{
+  cl_uint i;
+  cl_int err;
+
+  // Discover the number of platforms:
+  cl_uint nplatforms;
+  err = clGetPlatformIDs(0, NULL, &nplatforms);
+  check_error(err, "clGetPlatformIds");
+
+  // Now ask OpenCL for the platform IDs:
+  cl_platform_id* platforms = (cl_platform_id*)malloc(sizeof(cl_platform_id) * nplatforms);
+  err = clGetPlatformIDs(nplatforms, platforms, NULL);
+  check_error(err, "clGetPlatformIds");
+
+  // Ask OpenCL about each platform to understand the problem:
+  char name[128];
+  char vendor[128];
+  char version[128];
+
+  fprintf(stdout, "OpenCL reports %d platforms.\n\n", nplatforms);
+
+  for (i = 0; i < nplatforms; i++) {
+    err |= clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, 128, vendor, NULL);
+    err |= clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, 128, name, NULL);
+    err |= clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, 128, version, NULL);
+    check_error(err, "clGetPlatformInfo");
+
+    fprintf(stdout, "Platform %d: %s %s %s\n", i, vendor, name, version);
+  }
+
+  free(platforms);
+  return 0;
+}
+
+void SolverCL::init(size_t bufferSize) {
+  printPlatforms();
 
   cl_uint numPlatforms = 2;
   cl_platform_id platforms[2];
-  clGetPlatformIDs(numPlatforms, platforms, NULL);
+  cl_uint platformCount;
+  clGetPlatformIDs(numPlatforms, platforms, &platformCount);
+
+  cl_uint platformIndex = 0;
+
+  if (platformCount > 1) {
+    platformIndex = platformCount - 1;
+  }
+
+  //issues with
 
   // Connect to a compute device
   //
   cl_uint deviceCount = 0;
-  err = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_CPU, 1, &device_id, &deviceCount);
+  err = clGetDeviceIDs(platforms[platformIndex], CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &deviceCount);
   if (err != CL_SUCCESS)
   {
     printf("Error: Failed to create a device group!\n");
